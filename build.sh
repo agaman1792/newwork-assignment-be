e33aab28fe04cfd7c87af0976dcf3dee1ba7c9ba
Alexandru Gaman <alex@fogwarts.dev>
2025-11-17 23:22:03 +0200

    Added build script
    Updated readme

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-docker build -t ghcr.io/agaman1792/newwork-assignment-be:0.0.1 .
+set -x
+
+docker build \
+    -t ghcr.io/agaman1792/newwork-assignment-be:0.0.1 \
+    --label "org.opencontainers.image.source=https://github.com/agaman1792/newwork-assignment-be" \
+    --label "org.opencontainers.image.description=Newwork assignment back-end" \
+    --label "org.opencontainers.image.licenses=MIT" \
+    .
+
+set +x
